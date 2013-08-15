d0f66b17e6f674a7152232c58de214b2733fb59d
Oriol Ferrer Mesià <oriol@uri.cat>
2013-08-15 15:04:57 -0400

    added drawParagraph method

--- src/ofxFTGLFont.cpp
+++ src/ofxFTGLFont.cpp
@@ -1,99 +1,117 @@
 #include "ofxFTGLFont.h"
 
 ofxFTGLFont::ofxFTGLFont(){
-    loaded = false;
-    font = NULL;
+	loaded = false;
+	font = NULL;
 }
+
 
 ofxFTGLFont::~ofxFTGLFont(){
 	if(font != NULL){
-        //delete font;
-    }
+		//delete font;
+	}
 }
+
 
 bool ofxFTGLFont::loadFont(string filename, float fontsize, bool _bAntiAliased, bool _bFullCharacterSet, bool makeContours, float simplifyAmnt, int dpi){
 	fontsize *= 2;
-    font = new FTTextureFont(ofToDataPath(filename).c_str());
-//	lineHeight = fontsize * 1.43f;
-  	lineHeight = fontsize;
-  
-    font->Outset(0.0f, fontsize);
-
-    font->CharMap(ft_encoding_unicode);
-
-    if(font->Error()){
-        ofLogError("ofxFTGLFont") << "Error loading font " << filename;
-        delete font;
+	font = new FTTextureFont(ofToDataPath(filename).c_str());
+	
+	font->Outset(0.0f, fontsize);
+	
+	font->CharMap(ft_encoding_unicode);
+	
+	if(font->Error()){
+		ofLogError("ofxFTGLFont") << "Error loading font " << filename;
+		delete font;
 		return false;
-    }    
-    
-    if(!font->FaceSize(fontsize)){
-        ofLogError("ofxFTGLFont") << "Failed to set font size";
-        delete font;
+	}
+	
+	if(!font->FaceSize(fontsize)){
+		ofLogError("ofxFTGLFont") << "Failed to set font size";
+		delete font;
 		return false;
-    }
-    
-    loaded = true;
-    return true;
+	}
+	
+	layout.SetFont(font);
+	loaded = true;
+	return true;
 }
+
 
 float ofxFTGLFont::stringWidth(string c){
-    ofRectangle rect = getStringBoundingBox(c, 0,0);
-    return rect.width;
+	ofRectangle rect = getStringBoundingBox(c, 0, 0);
+	return rect.width;
 }
 
-float ofxFTGLFont::stringHeight(string c) {
-    ofRectangle rect = getStringBoundingBox(c, 0,0);
-    return rect.height;
+
+float ofxFTGLFont::stringHeight(string c){
+	ofRectangle rect = getStringBoundingBox(c, 0, 0);
+	return rect.height;
 }
+
 
 bool ofxFTGLFont::isLoaded(){
-    return loaded;
+	return loaded;
 }
+
 
 void ofxFTGLFont::setSize(int size){
-    if(loaded){
-	    font->FaceSize(size);
-    }
+	if(loaded){
+		font->FaceSize(size);
+	}
 }
 
-float ofxFTGLFont::getLineHeight(){
-	return lineHeight;
-}
-
-void ofxFTGLFont::setLineHeight(float newHeight){
-    lineHeight = newHeight;
-}
 
 ofRectangle ofxFTGLFont::getStringBoundingBox(string s, float x, float y){
-    if(loaded){
-    	FTBBox bbox = font->BBox(s.c_str());
-	    return ofRectangle(x + bbox.Lower().Xf(), y + bbox.Lower().Yf(), bbox.Upper().Xf(), bbox.Upper().Yf());
-    }
+	if(loaded){
+		FTBBox bbox = font->BBox(s.c_str());
+		return ofRectangle(x + bbox.Lower().Xf(), y + bbox.Lower().Yf(), bbox.Upper().Xf(), bbox.Upper().Yf());
+	}
 	return ofRectangle();
 }
+
 
 ofRectangle ofxFTGLFont::getStringBoundingBox(wstring s, float x, float y){
-    if(loaded){
-    	FTBBox bbox = font->BBox((wchar_t*)s.c_str());
-	    return ofRectangle(x + bbox.Lower().Xf(), y + bbox.Lower().Yf(), bbox.Upper().Xf(), bbox.Upper().Yf());
-    }
+	if(loaded){
+		FTBBox bbox = font->BBox((wchar_t *)s.c_str());
+		return ofRectangle(x + bbox.Lower().Xf(), y + bbox.Lower().Yf(), bbox.Upper().Xf(), bbox.Upper().Yf());
+	}
 	return ofRectangle();
 }
 
-void ofxFTGLFont::drawString(string s, float x, float y){
-    glPushMatrix();
-    glTranslatef(x, y, 0);
-    glScalef(1,-1,1);
 
-    font->Render(s.c_str());
-    glPopMatrix();
+void ofxFTGLFont::drawParagraph(string paragraph_, float x, float y, float boxWidth, float lineHeight, FTGL::TextAlignment align){
+	
+	layout.SetLineLength(boxWidth);
+	layout.SetAlignment(align);
+	layout.SetLineSpacing(lineHeight);
+	
+	glPushMatrix();
+	glTranslatef(x, y, 0);
+	ofLine(0, 0, boxWidth, 0);
+	glScalef(1, -1, 1);
+	layout.Render(paragraph_.c_str());
+	glPopMatrix();
 }
+
+
+void ofxFTGLFont::drawString(string s, float x, float y){
+	glPushMatrix();
+	glTranslatef(x, y, 0);
+	glScalef(1, -1, 1);
+	
+	font->Render(s.c_str());
+	glPopMatrix();
+}
+
 
 void ofxFTGLFont::drawString(wstring s, float x, float y){
-    glPushMatrix();
-    glTranslatef(x, y, 0);
-    glScalef(1,-1,1);
-    font->Render((wchar_t*)s.c_str());
-    glPopMatrix();
+	glPushMatrix();
+	glTranslatef(x, y, 0);
+	glScalef(1, -1, 1);
+	font->Render((wchar_t *)s.c_str());
+	glPopMatrix();
 }
+
+
--- src/ofxFTGLFont.h
+++ src/ofxFTGLFont.h
@@ -6,29 +6,34 @@
 #include "ftgl.h"
 
 class ofxFTGLFont {
-
-  public:
+	
+	enum ofxFTGLFontType {
+		
+	};
+public:
 	ofxFTGLFont();
-    ~ofxFTGLFont();
+	~ofxFTGLFont();
 	//the last parameters don't do anything, but are left in so that ofxFTGLFont
 	//can be dropped in in place of ofTrueTypeFont without compiler errors
-	bool 		loadFont(string filename, float fontsize = 10, bool _bAntiAliased = false, bool _bFullCharacterSet = false, bool makeContours = false, float simplifyAmnt = 0, int dpi = 72);
-    bool 		isLoaded();
-    
-	void 		setSize(int size);
-	float 		getLineHeight();
-    void		setLineHeight(float newHeight);
+	bool		loadFont(string filename, float fontsize = 10, bool _bAntiAliased = false, bool _bFullCharacterSet = false, bool makeContours = false, float simplifyAmnt = 0, int dpi = 72);
+	bool		isLoaded();
+	
+	void		setSize(int size);
+	
 	ofRectangle getStringBoundingBox(wstring s, float x, float y);
 	ofRectangle getStringBoundingBox(string s, float x, float y);
-
-	void 		drawString(wstring s, float x, float y);
-	void 		drawString(string s, float x, float y);
-	float 		stringHeight(string c);
-	float 		stringWidth(string c);
 	
-    FTFont*  font;
-  protected:
-    bool loaded;
-    float lineHeight;
+	void		drawString(wstring s, float x, float y);
+	void		drawString(string s, float x, float y);
+	float		stringHeight(string c);
+	float		stringWidth(string c);
+	
+	FTFont * font;
+	FTSimpleLayout layout;
+	
+	void drawParagraph(string text, float x, float y, float boxWidth, float lineHeight = 1.0f, FTGL::TextAlignment align = FTGL::ALIGN_LEFT);
+	
+protected:
+	bool loaded;
 };
 
